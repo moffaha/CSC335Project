34e88aded394d688040852540c3a816af4201920
moffaha <101844193+moffaha@users.noreply.github.com>
2023-06-01 15:17:43 +1200

    Version 2 of Menu class
    Includes Graphics to draw text to window. though unsuccsesful

--- Menu.java
+++ Menu.java
@@ -1,15 +1,16 @@
 /**
  *  Harvey Moffat
  *
- *  15/05/23
+ *  25/05/23
  *
- *  description
+ *  A class to add functionality to the display window
  */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Scanner;
 
 public class Menu extends JFrame implements ActionListener {
     //class variables
@@ -25,9 +26,9 @@ public class Menu extends JFrame implements ActionListener {
             case "Quit" : System.exit(0);
         }
     }
+
+
     public Menu(int width, int height) {
-
-
 
         setTitle("Menu Test");
         this.getContentPane().setPreferredSize(new Dimension(width,height));
@@ -36,11 +37,11 @@ public class Menu extends JFrame implements ActionListener {
 
         menuBar=new JMenuBar();
         this.setJMenuBar(menuBar);
-
+        //adding dropdown menu one
         menu = new JMenu("File");
         menuBar.add(menu);
 
-
+        //adding the dropdown menu options
         menuItem = new JMenuItem("First Menu Item");
         menuItem.addActionListener(this);
         menu.add(menuItem);
@@ -48,18 +49,26 @@ public class Menu extends JFrame implements ActionListener {
         menuItem.addActionListener(this);
         menu.add(menuItem);
 
-
+        //adding dropdown menu two
         menu = new JMenu("View");
         menuBar.add(menu);
 
+        //adding the dropdown menu option
         menuItem = new JMenuItem("Quit");
         menuItem.addActionListener(this);
         menu.add(menuItem);
 
+
         this.pack();
         this.toFront();
         this.setVisible(true);
+
     }
+
+    public void paint(Graphics g){
+        g.drawString("Hello Window",10,10);
+    }
+
 
 
 }

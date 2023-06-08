23926c5f105f85a0233294f19b820d5e423797a0
moffaha <101844193+moffaha@users.noreply.github.com>
2023-06-09 09:38:07 +1200

    Version 4 of Menu class
    started using JTextField instead of Graphics draw for text on window

---
+++ MenuV4.java
@@ -0,0 +1,90 @@
+/**
+ *  Harvey Moffat
+ *
+ *  08/06/23
+ *
+ *  A class to add functionality to the display window
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Scanner;
+
+public class Menu extends JFrame implements ActionListener {
+    //class variables
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+    JTextField textField;
+
+    public void actionPerformed(ActionEvent e) {
+
+        System.out.println(e);
+        String cmd = e.getActionCommand();
+        switch (cmd) {
+            case "Quit" : System.exit(0);//close the window when the Quit menu item is selected
+        }
+    }
+
+
+
+    public Menu(int width, int height) {
+
+        //set the title
+        setTitle("Menu Test");
+        this.getContentPane().setPreferredSize(new Dimension(width,height));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        menuBar=new JMenuBar();
+        this.setJMenuBar(menuBar);
+        //adding dropdown menu one
+        menu = new JMenu("File");
+        menuBar.add(menu);
+
+        //adding the dropdown menu options
+        menuItem = new JMenuItem("First Menu Item");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+        menuItem = new JMenuItem("Second Menu Item");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        //adding dropdown menu two
+        menu = new JMenu("View");
+        menuBar.add(menu);
+
+        //adding the dropdown menu option
+        menuItem = new JMenuItem("Quit");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        textField = new JTextField("Text Field Example");
+        textField.setBounds(50,50,200,30);
+        textField.setEditable(false);
+        add(textField);
+        setLayout(null);
+
+        //pack code and push it to the front of the screen
+        this.pack();
+        this.toFront();
+        this.setVisible(true);
+
+    }
+
+
+
+    /***
+    public void paint(Graphics g){
+        JPanel panel = new JPanel();
+        Graphics2D g2 = (Graphics2D)g;
+
+        Font font = new Font("Serif",Font.PLAIN,14);
+        g2.setFont(font);
+        g2.drawString("Hello Window",100,300);
+    }
+     ***/
+
+}

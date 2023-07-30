5243aa1ca5a3839cdd425154e9f07be9c488a1fe
moffaha <101844193+moffaha@users.noreply.github.com>
2023-07-30 23:22:45 +1200

    new main version
    clean up some of the extra code.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
 /** Author: Harvey Moffat
- *
- *  20/07/23
- *
- *  Creates window and runs algorithm
+ *  30/07/23
+ *  calls classes to run as an application
  */
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
-        Graph graph = new Graph();// Create a new graph
-
-        // Create three nodes (A, B, and C) and add them to the graph
-        Node nodeA = graph.addNode("A");
-        Node nodeB = graph.addNode("B");
-        Node nodeC = graph.addNode("C");
-
-        // Add edges with weights between the nodes to form a connected graph
-        graph.addEdge(nodeA, nodeB, 1);
-        graph.addEdge(nodeB, nodeC, 3);
-        graph.addEdge(nodeA, nodeC, 5);
-
-        // Use Dijkstra's algorithm to find the shortest paths from node A to all the other nodes
-        Map<Node, Integer> distances = Dijkstra.shortestPath(graph, nodeA);
-        System.out.println("Distance from A to B: " + distances.get(nodeB));// Print the shortest distances from node A to nodes B and C
-        System.out.println("Distance from A to C: " + distances.get(nodeC));
+        // Create and display the main window of the application.
+        Menu window = new Menu(600, 600);
+        window.setVisible(true);
     }
 }

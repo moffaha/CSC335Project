9a129dcd36c0fe658d89ae122b46f2af1cf9194c
moffaha <101844193+moffaha@users.noreply.github.com>
2023-07-30 23:31:46 +1200

    ReadCSV class now puts csv into array for graph class

--- ReadCSV.java
+++ ReadCSV.java
@@ -1,36 +1,40 @@
 /**
- *  Harvey Moffat
- *
- *  29/05/23
- *
- *  A class to read and print a CSV file
+ *  Author: Harvey Moffat
+ *  30/07/23
+ *  A class to read data from a CSV file and construct a graph based on the data.
  */
-
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ReadCSV
-{
-    public static void main(String[] args)
-    {
+public class ReadCSV {
+    public void readCSV(Graph graph) {
         String line = "";
         String splitBy = ",";
-        try
-        {
-            //parsing a CSV file into BufferedReader class constructor
-            BufferedReader br = new BufferedReader(new FileReader("CSVDemo.csv"));
-            while ((line = br.readLine()) != null)   //returns a Boolean value
-            {
-                String[] node = line.split(splitBy);    // use comma as separator
-                System.out.println(node[0] + " " + node[1] + " " + node[2] );
+        try {
+            // parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader("test.csv"));
+            while ((line = br.readLine()) != null) {   // returns a Boolean value
+                String[] data = line.split(splitBy);    // use comma as separator
+                String nodeID = data[0];
+                String neighborID = data[1];
+                int weight = Integer.parseInt(data[2]);
+
+                Node node = graph.getNode(nodeID);
+                if (node == null) {
+                    node = graph.addNode(nodeID);
+                }
+
+                Node neighbor = graph.getNode(neighborID);
+                if (neighbor == null) {
+                    neighbor = graph.addNode(neighborID);
+                }
+
+                graph.addEdge(node, neighbor, weight);
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-}  
+}

a2f4ec9fb0b35685574ba660c71cf40054b67467
moffaha <101844193+moffaha@users.noreply.github.com>
2023-09-21 15:18:13 +1200

    New version of ReadCSV
    Adding load file function

--- ReadCSV.java
+++ ReadCSV.java
@@ -1,25 +1,33 @@
 /**
  *  Author: Harvey Moffat
- *  30/07/23
+ *  21/09/23
  *  A class to read data from a CSV file and construct a graph based on the data.
  */
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ReadCSV {
-    public void readCSV(Graph graph) {
+    public void readCSV(Graph graph, File file) {
         String line = "";
         String splitBy = ",";
         try {
+            if (file == null) {
+                throw new IllegalArgumentException("CSV file is null.");
+            }
+
+            if (!file.exists()) {
+                throw new FileNotFoundException("CSV file does not exist: " + file.getAbsolutePath());
+            }
+
             // parsing a CSV file into BufferedReader class constructor
-            BufferedReader br = new BufferedReader(new FileReader("test.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("test2.csv"));
             while ((line = br.readLine()) != null) {   // returns a Boolean value
                 String[] data = line.split(splitBy);    // use comma as separator
                 String nodeID = data[0];
                 String neighborID = data[1];
                 int weight = Integer.parseInt(data[2]);
+
+
 
                 Node node = graph.getNode(nodeID);
                 if (node == null) {
@@ -32,9 +40,12 @@ public class ReadCSV {
                 }
 
                 graph.addEdge(node, neighbor, weight);
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
